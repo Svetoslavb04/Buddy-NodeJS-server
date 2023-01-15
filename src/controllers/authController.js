a61ea252fabbb0dbe92df09beaeff75eb4a1dce3
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-15 14:08:37 +0200

    feat: connect auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+
+router.post('/login', (req, res) => {
+
+})
+
+router.post('/register', (req, res) => {
+
+})
+
+router.get('/logout', (req, res) => {
+})
+
+module.exports = router
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/open-banking', require('./controllers/openBankingController'))
+router.use('/user', require('./controllers/authController'))
 
 module.exports = router;
