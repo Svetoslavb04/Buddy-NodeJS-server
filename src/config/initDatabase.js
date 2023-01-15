d6204e74676b59b44bf818ac0947efda076117bb
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-15 12:00:15 +0200

    refactor: server running

--- src/config/initDatabase.js
+++ src/config/initDatabase.js
@@ -2,4 +2,4 @@ const connectionString = process.env.DB_CONNECTION_STRING;
 
 const mongoose = require('mongoose');
 
-exports.connectDatabase = mongoose.connect(connectionString);
+exports.connectDatabase = () => mongoose.connect(connectionString);
