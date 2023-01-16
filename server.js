2ab0f22260747a74a97d97ab95ca5684d6cfcca6
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-16 18:00:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 require('dotenv').config();
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(require('cors')({
     origin: [process.env.ORIGIN],
