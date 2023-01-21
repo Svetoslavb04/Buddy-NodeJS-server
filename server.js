091312014591f20b71bed71e5bfa58fae527bc6f
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-01-21 12:58:06 +0200

    Update cookie settings

--- server.js
+++ server.js
@@ -13,16 +13,23 @@ app.use(require('cors')({
     credentials: true
 }))
 
-app.use(session({
+const sessionSettings = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     cookie: {
-        httpOnly: true,
-        secure: process.env.ENVIRONMENT !== 'development',
+        httpOnly: true
     },
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DB_CONNECTION_STRING, dbName: 'Budggy' })
-}))
+}
+
+if (process.env.ENVIRONMENT !== 'development') {
+    app.set('trust proxy', 1)
+    sessionSettings.cookie.secure = true
+    sessionSettings.cookie.sameSite = 'none'
+}
+
+app.use(session(sessionSettings))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
